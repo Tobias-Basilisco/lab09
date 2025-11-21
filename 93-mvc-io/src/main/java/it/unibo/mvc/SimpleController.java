9818b421fde3f1a09e817b8da2c9f8562f2ba596
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-11-21 14:04:27 +0100

    SimpleController all methods compile

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,25 +1,56 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+    private String strBuffer;
+    private List<String> history;
 
+    public SimpleController(){
+        history = new LinkedList<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void setStrBuffer(final String strToBuffer) throws NullPointerException{
+        if (strToBuffer == null){
+            throw new NullPointerException();
+        }
+        strBuffer = strToBuffer;
     }
 
+    @Override
     public String getStrBuffer(){
-        return null;
+        return Objects.requireNonNull(strBuffer);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public List<String> getPrintedStrings(){
-        return null;
+        return new ArrayList<>(history);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void print() throws IllegalStateException{
+        if (strBuffer == null || strBuffer.isEmpty()){
+            throw new IllegalStateException("Can't print empty buffer");
+        }
+        System.out.println(strBuffer);
+        history.add(strBuffer);
     }
 
 }
