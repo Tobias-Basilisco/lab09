010f22e7a255b395e3a275dc9f78dec7d8088e0a
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-11-21 13:44:22 +0100

    controller interface + empty implementation only signed

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,32 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+    /**
+     * sets the next string to print. 
+     * @throws NullPointerException
+     * @param strToBuffer
+     * 
+     */
+    void setStrBuffer(final String strToBuffer) throws NullPointerException;
 
+    /**
+     * @return the next string to print.
+     */
+    String getStrBuffer();
+
+    /**
+     * @return printed strings history
+     */
+    List<String> getPrintedStrings();
+
+    /**
+     * prints buffered string
+     * @throws IllegalStateException
+     */
+    void print() throws IllegalStateException;
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,25 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    public void setStrBuffer(final String strToBuffer) throws NullPointerException{
+    }
+
+    public String getStrBuffer(){
+        return null;
+    }
+
+    public List<String> getPrintedStrings(){
+        return null;
+    }
+
+    public void print() throws IllegalStateException{
+    }
 
 }
